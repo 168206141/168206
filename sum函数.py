9b8dc4b5c3c27236a6e6b8db2874080bc7bf234a
168206141 <44491927+168206141@users.noreply.github.com>
2018-11-05 14:08:06 +0800

    Create sum函数.py
    README.md

---
+++ sum函数.py
@@ -0,0 +1,17 @@
+def sum (alist,L=[]):
+    if len(L)==0:
+        L.append(0)
+    if len(alist) != 0:
+        L[0]+=alist[0]
+        alist.remove(alist[0])
+        return sum(alist)
+    return L[0]
+    
+alist = [1,2,2,4]
+s=sum(alist)
+print(s)
+    
+    
+    
+        
+       
