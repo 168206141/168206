a19cdd474cb1855f484ae6ef50418d8640ababf7
168206141 <44491927+168206141@users.noreply.github.com>
2019-01-01 19:37:36 +0800

    Create 偷钻石问题.py

---
+++ 168206141/偷钻石问题.py
@@ -0,0 +1,9 @@
+def found ():
+	    thief = ['A','B','C','D']
+	    #for(thief='A';thief<='D';thief++):
+	    for thief in ['A','B','C','D']:
+	        if(((thief!='A')+(thief=='D')+(thief=='B')+(thief!='D'))==1):
+	            print('小偷为'+thief)
+	            #print(thief)
+	
+	found ()
